3bd6b820d83957a1765ca662796fce5cf6cb2305
AshkenSC <393940378@qq.com>
2019-01-14 12:11:52 +0800

    Implementing Bullet class, update function complete

--- bullet.py
+++ bullet.py
@@ -9,14 +9,23 @@ class Bullet(Sprite):
         super(Bullet, self).__init__()
         self.screen = screen
 
-        # create a rect at (0,0), then set correct location
+        # at first, create a rect at (0,0)
+        # then set it to correct location (ship top)
         self.rect = pygame.Rect(0, 0, ai_settings.bullet_width,
                                 ai_settings.bullet_height)
         self.rect.centerx = ship.rect.centerx
         self.rect.top = ship.rect.top
 
-        # store bullets' location in FLOAT type
+        # store bullet location in FLOAT type
+        # to adjust bullet location more delicately
         self.y = float(self.rect.y)
 
         self.color = ai_settings.bullet_color
         self.speed_factor = ai_settings.bullet_speed_factor
+
+    def update(self):
+        '''move bullet upwards'''
+        # update FLOAT value representing bullet location
+        self.y -= self.speed_factor
+        # update location of the rectangle that represents bullet
+        self.rect.y = self.y
